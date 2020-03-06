088865a9ef2ea6004fd9d3a45b705a74eb8549ef
ma.savoyat@gmail.com <ma.savoyat@gmail.com>
2020-03-06 07:19:22 +0100

    Correction to Readme. ChannelDecoder work in progress.

--- ChannelDecoder.py
+++ ChannelDecoder.py
@@ -16,6 +16,12 @@ class ChannelDecoder:
     and sample_rate is 250KHz."""
     
     def __init__(self, radio_recevier : RadioReceiver, channel):
+        if radio_recevier.center_freq != 446.1e6:
+            raise ValueError("RadioRecevier shall have 446.1 MHz center frequency")
+            return
+        if radio_recevier.sample_rate != 250e3:
+            raise ValueError("RadioRecevier shall have 250 KHz sample rate")
+            return
         self._thread = threading.Thread(target=self._main_loop)
         self._run = threading.Event()
         self._radio_recevier = radio_recevier
@@ -25,10 +31,11 @@ class ChannelDecoder:
         self._thread.start()
         
     def _main_loop(self):
-        channel_freq = 446e6 + 3.125e3 + 6.25e3*self._channel
+        n = 80
+        channel_freq = 446e6 + 6.25e3 + 12.5e3*self._channel
         lo_freq = channel_freq - self._radio_recevier.center_freq
         samp_rate = self._radio_recevier.sample_rate
-        lo = np.exp(1j*lo_freq/samp_rate)
+        lo = np.exp(-2*np.pi*1j*lo_freq/samp_rate*np.arange(n))
         buffer = list() # local buffer used for group data processing
         self._radio_recevier.registerQueue(self._queue)
         while self._run.is_set():
@@ -37,10 +44,12 @@ class ChannelDecoder:
                 self._run.clear()
                 continue
             buffer.extend(data)
-            # Process data by 80*16 samples so that is can be mixed and downsampled by packet
-            while len(buffer)>80*16:
-                
-                del buffer[:80*16]
+            # Process data by 80 samples
+            while len(buffer)>n:
+                data_mix = buffer[:n]*lo
+                data_filt = 
+                data_decim = data_filt[8::16]
+                del buffer[:n]
         
         self._radio_recevier.unregisterQueue(self._queue)
         
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ For librtlsdr on windows: https://ftp.osmocom.org/binaries/windows/rtl-sdr/
 
 ## Frequency map
 Receiver center frequency: 446.1 MHz
-Receiver sample rate: 250 ksps ( 80 x 3.125 KHz )
+
+Receiver sample rate: 250 ksps ( 40 x 6.25 KHz )
 
 Channel sample rate: 15.625 ksps ( receiver rate decimate by 16 and rate close to 16 ksps )
