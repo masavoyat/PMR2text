a6a21b13813be886b8f75074b2e836aa2cdfa4bc
ma.savoyat@gmail.com <ma.savoyat@gmail.com>
2020-03-09 14:27:03 +0100

    Implement FMdemodulator

--- FMdemodulator.py
+++ FMdemodulator.py
@@ -11,11 +11,17 @@ class FMdemodulator:
     
     def __init__(self, dt):
         self.z1 = 0+0j
-        self.dt = dt
+        self.fs = 1/dt
         
     def filt(self, data):
-        # w = 2/dt*abs(z1-z2)/abs(z1+z2)
+        # w = 2*fs*IM((z1-z2)*(z1+z2).conjugate())/((z1+z2)*(z1+z2).conjugate())
         out = list()
         for z0 in data:
+            m = z0 - self.z1
+            p = z0 + self.z1
+            projection = (m*p.conjugate()).imag
+            norm2 = (p*p.conjugate()).real
+            w = 2*self.fs*projection/norm2
+            out.append(w)
             self.z1 = z0
             
